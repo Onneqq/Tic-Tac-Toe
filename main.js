e86f74b83f8c3bbb59e2984c5bbbd3d149e779d9
adampenno <adampenno93@gmail.com>
2022-11-22 15:53:32 +1100

    Basic HTML & CSS structure complete - JS planning started

---
+++ main.js
@@ -0,0 +1,16 @@
+// Planning
+
+// 1.) Potential Variables
+//  - Player turn - toggle between class applied to dom object - create css classes (default, x, o)
+//  - Win condition met - boolean
+//  -
+
+// 2.) Click on box => change class/background image - may need to use class/id to be able to check win condition
+// DOM - event.target - check current class and apply class on that condition
+
+// 3.) run prompt once won displaying winner, current tally and play again option or quit
+
+// Extras:
+// Keep track of player scores over multiple rounds (2 vars)
+// time limit per turn
+// add CSS animations etc
